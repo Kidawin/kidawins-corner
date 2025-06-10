d089cf8ae4187aafd95b928ee83cb9bab9fa55f4
Kidawin <doomblade721@gmail.com>
2025-06-09 20:54:28 -0400

    updated looks, added pages, added top navbar started credits page and added a few other blank pages added a navbar that will show on any page added tomaNOTchi pet for testing separated Main into different Articles for a clean look

---
+++ topbar.js
@@ -0,0 +1,12 @@
+
+var html = '<div class="topbar"><a href="index.html">Home</a></div>';
+
+html += '<div class="topbar"><a href="/works.html">My Works</a></div>'
+var section1 = ['Media','Credits']//write one-word link names in this list
+for (i = 0; i<section1.length; i++){
+	html += '<div class="topbar"><a href="' + section1[i] + '.html">' + section1[i] + '</a></div>'
+}
+
+
+
+document.getElementById("topbar").innerHTML = html;
