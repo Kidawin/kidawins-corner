b72e987da49c9743f57614141799d18f1bd74763
Kidawin <doomblade721@gmail.com>
2025-06-10 22:48:09 -0400

    sidebar, guestbook, banner, folders

--- topbar.js
+++ topbar.js
@@ -1,7 +1,7 @@
 
 var html = '<div class="topbar"><a href="index.html">Home</a></div>';
 
-html += '<div class="topbar"><a href="/works.html">My Works</a></div>'
+html += '<div class="topbar"><a href="/works.html">My Stuff</a></div>'
 var section1 = ['Media','Credits']//write one-word link names in this list
 for (i = 0; i<section1.length; i++){
 	html += '<div class="topbar"><a href="' + section1[i] + '.html">' + section1[i] + '</a></div>'
